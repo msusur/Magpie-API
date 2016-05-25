42243e8ea0e47a989c904a7f176c25f5d30db8aa
msusur <mail@mertsusur.com>
2016-05-25 08:27:08 +0300

    npm & basic tests

---
+++ source/Magpie.Library.Tests/HtmlParserTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace Magpie.Library.Tests
+{
+    public class HtmlParserTests
+    {
+        [Fact]
+        public void T()
+        {
+            HtmlParser parser = new HtmlParser();
+            var command = ParseCommand.Create();
+            parser.ParseModel(command);
+        }
+    }
+
+    public class HtmlParser
+    {
+        public void ParseModel(ParseCommand parseCommand)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class ParseCommand
+    {
+        ParseCommand()
+        {
+            
+        }
+
+        public static ParseCommand Create()
+        {
+            return new ParseCommand();    
+        }
+    }
+}
